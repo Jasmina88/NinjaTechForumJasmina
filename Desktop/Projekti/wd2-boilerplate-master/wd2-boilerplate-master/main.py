ac0894d9d035eb9d8e924024c00b4af7ac692883
JasminaD <dzelsomina@gmail.com>
2017-03-30 15:37:32 +0200

    final updates

--- Desktop/Projekti/wd2-boilerplate-master/wd2-boilerplate-master/main.py
+++ Desktop/Projekti/wd2-boilerplate-master/wd2-boilerplate-master/main.py
@@ -3,16 +3,25 @@ import os
 import jinja2
 import webapp2
 import webapp2
+
+from crons.delete_topics_cron import DeleteTopicsCron
 from handlers.base import MainHandler, AboutHandler, CookieHandler
-from handlers.topics import TopicCreateHandler, TopicDetailsHandler
+from handlers.topics import TopicCreateHandler, TopicDetailsHandler, TopicDeleteHandler
 from handlers.comments import CommentCreateHandler
+from workers.email_comment_worker import EmailNewCommentWorker
 
 app = webapp2.WSGIApplication([
 
 webapp2.Route('/', MainHandler, name="main-page"),
 webapp2.Route('/about', AboutHandler, name="about-page"),
-webapp2.Route('/set-cookie', CookieHandler, name="nastavi-cookie"),
+webapp2.Route('/set-cookie', CookieHandler, name="set-cookie"),
 webapp2.Route('/topic/create', TopicCreateHandler, name="topic_create"),
 webapp2.Route('/topic/<topic_id:\d+>', TopicDetailsHandler, name="topic_details"),
 webapp2.Route('/topic/<topic_id:\d+>/comment/add', CommentCreateHandler, name="comment_create"),
+webapp2.Route('/topic/<topic_id:\d+>/delete', TopicDeleteHandler, name="topic-delete"),
+
+webapp2.Route("/task/email-new-comment", EmailNewCommentWorker),
+
+#CRON
+webapp2.Route('/cron/delete-topics', DeleteTopicsCron, name="cron-delete-topics"),
 ], debug=True)
